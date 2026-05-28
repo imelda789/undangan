397e8d82b51a171cfae1c0eee3dfc3b6c6ff2553
imelda789 <imeldajanuarisma@gmail.com>
2026-05-28 10:18:00 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const targetDate = new Date("December 12, 2026 08:00:00").getTime();
+
+const countdown = setInterval(function () {
+  const now = new Date().getTime();
+
+  const distance = targetDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+}, 1000);
